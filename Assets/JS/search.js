5ad990f72530b8d66d5817ed86e28518e31a16f8
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-02-27 11:58:02 -0600

    w

--- Assets/JS/search.js
+++ Assets/JS/search.js
@@ -51,7 +51,7 @@ function search_jsonData() {
              The User Experience Designer position exists to create compelling and digital user experience through excellent design...
             </div>
             <div class="job-detail-buttons">
-             <button class="search-buttons detail-button">Full Time</button>
+             <button class="search-buttons detail-button">${obj.Type}</button>
              <button class="search-buttons detail-button">Min. 1 Year</button>
              <button class="search-buttons detail-button">Senior Level</button>
             </div>
