d92526ac6010ff5f351ac664c6a414f21820949a
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-02-27 11:30:09 -0600

    d

--- Assets/JS/search.js
+++ Assets/JS/search.js
@@ -34,35 +34,32 @@ function search_jsonData() {
             const elem = document.createElement("p")
 
             elem.innerHTML = `
-                <div class="job-card">
+            <div class="job-card">
                 <div class="job-card-header">
-                    <svg viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg"
-                        style="background-color:#2e2882">
+                    <svg viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg" style="background-color:#2e2882">
                         <g fill="#feb0a5">
-                            <path
-                                d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0" />
+                            <path d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0" />
                             <path d="M127.7 394.1l128.4 92 128.3-92-128.3-92zm0 0" />
                         </g>
-                        <path
-                            d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0"
-                            fill="#feb0a5" />
+                        <path d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0" fill="#feb0a5" />
                     </svg>
-                    <div class="menu-dot"></div>
-                </div>
-                <div class="job-card-title">${obj.Name}</div>
-                <div class="job-card-subtitle">
-                    ${obj.Description}
-                </div>
-                <div class="job-detail-buttons">
-                    <button class="search-buttons detail-button">${obj.Name}</button>
-                    <button class="search-buttons detail-button">${obj.Type}</button>
-                    <button class="search-buttons detail-button">${obj.Status}</button>
-                </div>
-                <div class="job-card-buttons">
-                    <a href="${obj.Play}"><button class="search-buttons card-buttons">Play Game</button></a>
-                    <a href="${obj.Open}"><button class="search-buttons card-buttons-msg">Information</button></a>
-                </div>
+                <div class="menu-dot"></div>
             </div>
+
+            <div class="job-card-title">${obj.Name}</div>
+            <div class="job-card-subtitle">
+             The User Experience Designer position exists to create compelling and digital user experience through excellent design...
+            </div>
+            <div class="job-detail-buttons">
+             <button class="search-buttons detail-button">Full Time</button>
+             <button class="search-buttons detail-button">Min. 1 Year</button>
+             <button class="search-buttons detail-button">Senior Level</button>
+            </div>
+            <div class="job-card-buttons">
+             <button class="search-buttons card-buttons">Apply Now</button>
+             <button class="search-buttons card-buttons-msg">Messages</button>
+            </div>
+           </div>
                 `
             x.appendChild(elem)
         }
