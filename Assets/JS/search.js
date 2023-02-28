414833e3476163363595a1978bfd63647e15c05a
Mike <124840291+mikewashereGH@users.noreply.github.com>
2023-02-28 06:34:39 -0600

    g

--- Assets/JS/search.js
+++ Assets/JS/search.js
@@ -15,7 +15,7 @@ let jsonData = `[
         "Name": "Multiplication",
         "Type": "Math",
         "Description": "A math game that is based on multiplication. It allows students to practice multiplication, while improving their speed and accuracy.",
-        "Preview": "https://play-lh.googleusercontent.com/V31nfvzNcu_47lr8y2L7m3uw9azrpGyox88gpzhQxwfZSXliFJmIRVSlYJL1eOo_X2U",
+        "Preview": "https://play-lh.googleusercontent.com/M9xYsC4u-_LEfG7DeNeSGfgi7XXF0JajLfORnm9CoHGh9Vn5o_IH8V9aHUAmFEYA1Q=w240-h480-rw",
         "Play": "../Pages/Games/Multiplication/index.html",
         "Open": "home.html",
         "Date": "02/27/23",
@@ -48,13 +48,14 @@ function search_jsonData() {
             elem.innerHTML = `
             <div class="job-card">
                 <div class="job-card-header">
-                    <svg viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg" style="background-color:#2e2882">
+                    <!-- <svg viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg" style="background-color:#2e2882">
                         <g fill="#feb0a5">
                             <path d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0" />
                             <path d="M127.7 394.1l128.4 92 128.3-92-128.3-92zm0 0" />
                         </g>
                         <path d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0" fill="#feb0a5" />
-                    </svg>
+                    </svg> -->
+                    <img src="${obj.Preview}">
                 <div class="menu-dot"></div>
             </div>
 
